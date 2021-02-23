12e9eb8b15d6943e5c117ccbeb8252766f0514c3
Malvika Viswanathan <mviswana@purdue.edu>
2021-02-22 19:06:56 -0500

    Code for Clean Hand Image

--- Spring2021/xyzm.m
+++ Spring2021/xyzm.m
@@ -1,14 +1,19 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This program is for the processing of 3D hand data of the xyzm format and
-% reduces noise. The working part is in the section that says, "THIS IS THE
-% ACTUAL WORKING PART".
-% Input required - xyzm file
-% Output - Clean hand image
-% Author: Slight of Hand ( Sriram,Zi,Malvika,Hyun).
+% reduces noise. It also converts it back into an RGB image and saves it in
+% the same name.
+%
+% Input required: xyzm file
+%
+% Output: Clean hand image, saved under the same name
+%
+% Semester: Fall 2020
+%
+% Author: Slight of Hand (Sriram,Zi,Malvika,Hyun).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% THIS IS THE ACTUAL WORKING PART
 
-[x, y, z, r, g, b, mask] = xyzmread("hand 12.xyzm"); % function call
+[x, y, z, r, g, b, mask] = xyzmread("hand 1.xyzm"); % function call
+[filepath,name,ext] = fileparts("hand 1.xyzm");
 
 % getting the size of the picture
 colRange = 1:size(x, 1);     
@@ -17,8 +22,10 @@ figure(1)
 imagesc(mask(colRange, rowRange)) % inbuilt function to display image
 
 % code for removing background noise
-for i = 1:480
-    for j = 1:640
+rows = size(x,1);                  % number of rows
+cols = size(x,2);                  % number of columns
+for i = 1:rows
+    for j = 1:cols
         if(b(i,j) <= 65)            %% all our noise is blue
             z(i,j) = 0;
             x(i,j) = 0;
@@ -27,6 +34,7 @@ for i = 1:480
     end
 end
 
+
 % Image segmentation
 z_seg = xyzmSeg(z); % function call    
 figure(2);
@@ -34,66 +42,42 @@ imagesc(z_seg)
 xlabel("x");
 ylabel("y");
 
-image_denoise = image_denoise_gray_3x3(y_seg); % removing noise 
+% creating a binary matrix
+bin_mat = zeros(size(z_seg));           % creates a matrix of 480x640
+for i = 1:rows
+    for j = 1:cols
+        if(z_seg(i,j) == 255)
+            bin_mat(i,j) = 1;           % hand part is 1, basically white
+        else
+            bin_mat(i,j) = 0;           % else 0, basically black, the bg
+        end
+    end
+end
+
+% since we know have a mask of the hand, we can create the R,G,B vectors of
+% the image  by simply multiplying the intial r,g,b vectors with the binary
+% matrix.
+r_mat = r .* bin_mat;
+g_mat = g .* bin_mat;
+b_mat = b .* bin_mat;
+
+% creating the RGB image
+% since our image consists of r,g,b vectors, we need to concatenate the
+% three to make an image of the size 480x640x3
+image = cat(3, r_mat, g_mat, b_mat);
+image_bin = uint8(image);
 figure(3);
-imshow(image_denoise);
-figure(4);
-surf(y);
-xlabel("x");
-ylabel("y");
-zlabel("z");
+imshow(image_bin);
+
+% saving the image
+filename_bin = strcat(name, ".png");
+imwrite(image_bin,filename_bin);
+
+% figure(4);
+% surf(y);
+% xlabel("x");
+% ylabel("y");
+% zlabel("z");
 % If you look at the surf image we can see that the hand wasn't straight
 % and it was slightly tilted forward. All this can be avoided if look at
 % the image from the depth point of view.
-
-%% Trial 1
-[x, y, z, r, g, b, mask] = xyzmread("hand1.xyzm");
-
-colRange = 1:size(x, 1);
-rowRange = 1:size(x, 2);
-figure
-hold on
-surf(x(colRange, rowRange),y(colRange, rowRange),-z(colRange, rowRange),  'FaceColor', 'interp',...
-    'EdgeColor', 'none',...
-    'FaceLighting', 'phong');
-set(gca, 'DataAspectRatio', [1, 1, 1])
-zlim([-900 -700]);
-caxis([-900 -700]);
-colorbar;
-camlight left;
-view(-270, 0);
-
-%% Trial 3
-[x, y, z, r, g, b, mask] = xyzmread("hand 1.xyzm");
-
-colRange = 1:size(x, 1);
-rowRange = 1:size(x, 2);
-figure(1)
-imagesc(y(colRange, rowRange))
-
-for i=1:length(y(1,:))
-    for j=1:length(y(1,:))
-        if abs(y(i,j))>0
-            y(i,j)=255;
-        else 
-            y(i,j) = 0;
-        end 
-    end 
-end 
-figure(4)
-imagesc(y(colRange,rowRange))    
-figure(5)
-imshow(y(colRange,rowRange))
-%% Trial 11 -  with shoe image to check our segmentation algorithm.
-[x, y, z, r, g, b, mask] = xyzmread("shoe2_high_res_processed.xyzm");
-
-colRange = 1:size(x, 1);
-rowRange = 1:size(x, 2);
-figure(1)
-imagesc(mask(colRange, rowRange))
-
-y_seg = xyzmSeg(mask);
-figure(2);
-imagesc(y_seg(colRange, rowRange))
-figure(3);
-imshow(y_seg)
